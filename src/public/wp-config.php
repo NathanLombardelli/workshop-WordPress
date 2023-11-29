7211fe67ca6ae5738b6eed5e66c9f5bdfab96d4c
NicolasJamar <nicolas.jamar@gmail.com>
2023-11-29 11:28:50 +0100

    install ACF PRO

--- src/public/wp-config.php
+++ src/public/wp-config.php
@@ -20,22 +20,24 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'designo' );
 
 /** Database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', 'pass1234' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'mysql' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
 
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
+
+define('FS_METHOD','direct');
 
 /**#@+
  * Authentication unique keys and salts.
